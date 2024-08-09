eb1cc4407fedfd41789e6e27b1ed1f7de110f88f
𝐒𝐎𝐔𝐑𝐀𝐁𝐇 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 <163111114+PROFESSOR-SOURABH@users.noreply.github.com>
2024-08-09 11:12:14 +0530

    Update stream.py

--- PROFESSOR/utils/stream/stream.py
+++ PROFESSOR/utils/stream/stream.py
@@ -1,16 +1,22 @@
 import os
+import logging
 from random import randint
 from typing import Union
 
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from PROFESSOR import Carbon, YouTube, app
+from PROFESSOR import Carbon, YouTube, YTB, app
 from PROFESSOR.core.call import PROF
 from PROFESSOR.misc import db
 from PROFESSOR.utils.database import add_active_video_chat, is_active_chat
 from PROFESSOR.utils.exceptions import AssistantErr
-from PROFESSOR.utils.inline import aq_markup, close_markup, stream_markup
+from PROFESSOR.utils.inline import (
+    aq_markup,
+    close_markup,
+    stream_markup,
+    telegram_markup,
+)
 from PROFESSOR.utils.pastebin import PROFBin
 from PROFESSOR.utils.stream.queue import put_queue, put_queue_index
 from PROFESSOR.utils.thumbnails import get_thumb
@@ -78,7 +84,14 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-                    raise AssistantErr(_["play_14"])
+                    try:
+                        
+                        file_path, direct = await YTB.download(
+                            vidid, mystic, video=status, videoid=True
+                        )
+                    except Exception as e:
+                        logging.error(e)
+                        raise AssistantErr(_["play_14"])
                 await PROF.join_call(
                     chat_id,
                     original_chat_id,
@@ -99,7 +112,7 @@ async def stream(
                     forceplay=forceplay,
                 )
                 img = await get_thumb(vidid)
-                button = stream_markup(_, chat_id)
+                button = stream_markup(_, vidid, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
@@ -142,7 +155,13 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-            raise AssistantErr(_["play_14"])
+            try:
+                file_path, direct = await YTB.download(
+                    vidid, mystic, videoid=True, video=status
+                    )
+            except Exception as e:
+                logging.error(e)
+                raise AssistantErr(_["play_14"])
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -185,7 +204,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = stream_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -238,7 +257,7 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
@@ -292,7 +311,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
@@ -353,7 +372,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -408,7 +427,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
